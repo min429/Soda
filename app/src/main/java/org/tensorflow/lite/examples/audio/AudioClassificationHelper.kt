04760b7d322751a85d86738653ca22e0df57f16b
Seungmin Lee <lsm990429@gmail.com>
2023-07-06 18:27:52 +0900

    SoundCheckHelper 생성

--- app/src/main/java/org/tensorflow/lite/examples/audio/AudioClassificationHelper.kt
+++ app/src/main/java/org/tensorflow/lite/examples/audio/AudioClassificationHelper.kt
@@ -18,12 +18,8 @@ package org.tensorflow.lite.examples.audio
 
 import android.content.Context
 import android.media.AudioRecord
-import android.os.Build
 import android.os.SystemClock
-import android.os.VibrationEffect
-import android.os.Vibrator
 import android.util.Log
-import androidx.core.content.ContextCompat
 import java.util.concurrent.ScheduledThreadPoolExecutor
 import java.util.concurrent.TimeUnit
 import org.tensorflow.lite.examples.audio.fragments.AudioClassificationListener
@@ -31,11 +27,7 @@ import org.tensorflow.lite.support.audio.TensorAudio
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import org.tensorflow.lite.task.audio.classifier.AudioClassifier
 import org.tensorflow.lite.task.core.BaseOptions
-import kotlin.math.ln
-import kotlin.math.log10
 import kotlin.properties.Delegates
-
-private const val TAG = "AudioClassificationHelp"
 
 class AudioClassificationHelper(
     val context: Context,
@@ -52,7 +44,6 @@ class AudioClassificationHelper(
     private lateinit var recorder: AudioRecord
     private lateinit var executor: ScheduledThreadPoolExecutor
     private lateinit var soundCheckExecutor: ScheduledThreadPoolExecutor
-    private lateinit var buffer: TensorBuffer
     private var bytesRead by Delegates.notNull<Int>()
 
     private val classifyRunnable = Runnable {
@@ -61,7 +52,7 @@ class AudioClassificationHelper(
 
     // 새로운 스레드에서 소리 크기 확인 및 진동 울리기
     private val soundCheckRunnable = Runnable {
-        soundCheck()
+        SoundCheckHelper.soundCheck(tensorAudio, bytesRead, context)
     }
 
     init {
@@ -149,51 +140,9 @@ class AudioClassificationHelper(
         listener.onResult(output[0].categories, inferenceTime)
     }
 
-    private fun soundCheck() {
-        buffer = tensorAudio.tensorBuffer
-
-        // 버퍼에서 읽은 데이터를 소리 크기로 변환하여 확인합니다
-        var soundAmplitude = 0.0
-        for (i in 0 until bytesRead) {
-            val sample = buffer.getFloatValue(i)
-            soundAmplitude += sample * sample
-        }
-
-        // Root Mean Square (RMS) 계산하기
-        val rms = kotlin.math.sqrt(soundAmplitude / bytesRead)
-
-        // RMS를 데시벨로 변환하기
-        // 0에 로그를 취하는 것을 방지하기 위해 1e-7 추가하기
-        val soundDecibel = 30 * log10(rms * 5000 + 1e-7)
-        Log.d(TAG, "soundDecibel: $soundDecibel")
-
-        // 소리 크기가 100데시벨 이상인 경우 핸드폰에 진동을 울리는 코드를 작성합니다
-        if (soundDecibel >= 100) {
-            // 진동을 울리는 코드를 작성합니다
-            try {
-                viberate()
-            } catch (e: Exception) {
-                Log.e(TAG, "Error occurred while vibrating", e)
-                val exceptionMessage = e.message
-                Log.e(TAG, "Exception message: $exceptionMessage")
-            }
-        }
-    }
-
     fun stopAudioClassification() {
         recorder.stop()
         executor.shutdownNow()
-    }
-
-    private fun viberate(){
-        val vibrator = ContextCompat.getSystemService(context, Vibrator::class.java) as Vibrator
-        if (vibrator.hasVibrator()) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                vibrator.vibrate(VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE))
-            } else {
-                vibrator.vibrate(1000)
-            }
-        }
     }
 
     companion object {
@@ -205,4 +154,5 @@ class AudioClassificationHelper(
         const val YAMNET_MODEL = "yamnet.tflite"
         const val SPEECH_COMMAND_MODEL = "speech.tflite"
     }
+
 }
