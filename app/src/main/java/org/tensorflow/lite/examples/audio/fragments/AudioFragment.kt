997193237da4e7784a504b623d1055846b3de95a
Proverbs13 <ichan0318@naver.com>
2023-07-15 00:03:51 +0900

    STT 녹음버튼으로 통합
    녹음 외 기타 코드 주석으로 남겨둠

--- app/src/main/java/org/tensorflow/lite/examples/audio/fragments/AudioFragment.kt
+++ app/src/main/java/org/tensorflow/lite/examples/audio/fragments/AudioFragment.kt
@@ -16,13 +16,10 @@
 
 package org.tensorflow.lite.examples.audio.fragments
 
-
 import android.content.Intent
 import android.media.MediaRecorder
 import android.media.MediaScannerConnection
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.speech.RecognizerIntent
 import android.util.Log
 import android.view.LayoutInflater
@@ -39,20 +36,21 @@ import org.tensorflow.lite.examples.audio.R
 import org.tensorflow.lite.examples.audio.databinding.FragmentAudioBinding
 import org.tensorflow.lite.examples.audio.ui.ProbabilitiesAdapter
 import org.tensorflow.lite.support.label.Category
-import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.ByteArrayOutputStream
 import java.io.FileOutputStream
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
 import java.util.Locale
+import android.net.Uri
+import android.os.Environment
 
 //stt
-
-import android.Manifest
-import android.content.pm.PackageManager
 import android.speech.RecognitionListener
 import android.speech.SpeechRecognizer
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
@@ -73,11 +71,12 @@ class AudioFragment : Fragment() {
 
     // 스위치 & 버튼용
     private var isSwitchOn = true
-    private var isRecording = false
+    //private var isRecording = false //- 녹음 버튼 통합으로 코드 통일
 
     //stt 용
     private lateinit var speechRecognizer: SpeechRecognizer
     private var isListening = false // 음성 인식 중 여부를 추적하는 플래그 변수
+    private var duplication_check = false // 음성인식 시작 중복 처리 플래그 변수
     val recognizedText = mutableListOf<String>() // 수정: recognizedText 변수를 리스트로 선언하고 초기화
     //private var recognizedText: String = "" // 음성 인식 결과를 저장하는 변수
     //
@@ -105,8 +104,6 @@ class AudioFragment : Fragment() {
     }
 
 
-
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -121,6 +118,7 @@ class AudioFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         // 어댑터 설정
         fragmentAudioBinding.recyclerView.adapter = adapter
+
         // AudioClassificationHelper 객체 생성 및 초기화
         audioHelper = AudioClassificationHelper(
             requireContext(),
@@ -131,8 +129,10 @@ class AudioFragment : Fragment() {
         // 스위치 + STT 녹음 버튼------------------------------------------------------------------------
         val switch = fragmentAudioBinding.switchButton
         val recordButton = view.findViewById<Button>(R.id.record_button)
-        val sttButton=view.findViewById<Button>(R.id.stt_button)
+
         val stttext = fragmentAudioBinding.sttText
+        //
+        val sttButton=view.findViewById<Button>(R.id.stt_button)
 
         //스위치 클릭
         switch.setOnCheckedChangeListener { _, isChecked ->
@@ -156,19 +156,52 @@ class AudioFragment : Fragment() {
             }
         }
 
-        // 녹음 버튼 클릭
+        // 녹음 버튼 클릭------------------------------------------------------------------------
         recordButton.setOnClickListener {
             Log.e("스위치 여부", "${isSwitchOn}")
             if (!isSwitchOn) { // 녹음 스위치 off 상태에서만 작동
-                if (isRecording) {
-                    stopRecording()
-                    isRecording = false
-                    recordButton.text = "녹음 시작" // 녹음 중지 시 버튼 텍스트 변경
-                } else {
-                    startRecording()
-                    isRecording = true
-                    recordButton.text = "녹음 중지" // 녹음 시작 시 버튼 텍스트 변경
+
+                if (!isListening) { // 음성 인식 중이 아닌 경우에만 음성인식 시작
+                    isListening = true
+                    val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR")
+                    //intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, 5000); // 녹음 기한 5초
+                    //intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, 5000); // 녹음 기한 5분
+
+                    speechRecognizer.startListening(intent)
+                    recordButton.text = "녹음 중지" // stt녹음 시작 시 버튼 텍스트 변경
+
+                    Log.d("isListening", isListening.toString())
+                    Log.d("stt", "stt 음성인식 시작")
+
+
+                } else { // 음성 인식 중이면 음성인식 종료
+                    isListening = false
+                    speechRecognizer.cancel()
+                    recordButton.text = "녹음 시작" // stt녹음 중지 시 버튼 텍스트 변경
+                    val text = recognizedText.joinToString(separator = " ")
+                    stttext.setText(text)
+                    recognizedText.clear() // 다음 인식을 위해 비우기
+
+                    Log.d("isListening", isListening.toString())
+                    Log.d("stt", "stt 음성인식 중단")
+
+                    duplication_check=false
                 }
+
+                  // 녹음 버튼 통합으로 임시 주석 처리
+//                if (isRecording) {
+//                    stopRecording()
+//                    isRecording = false
+//                    recordButton.text = "녹음 시작" // 녹음 중지 시 버튼 텍스트 변경
+//                } else {
+//                    startRecording()
+//                    isRecording = true
+//                    recordButton.text = "녹음 중지" // 녹음 시작 시 버튼 텍스트 변경
+//                }
+
+
             }
             else{
                 Toast.makeText(recordButton.context, "수동녹음을 하려면 자동녹음 스위치를 off로 해주세요", Toast.LENGTH_SHORT).show()
@@ -177,19 +210,20 @@ class AudioFragment : Fragment() {
         }
 
 
-//        // 권한 확인
-//        val permission = Manifest.permission.RECORD_AUDIO
-//        if (ActivityCompat.checkSelfPermission(requireActivity(), permission) != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(requireActivity(), arrayOf(permission), 0)
-//        }
-
-
-        // SpeechRecognizer 초기화
+        // SpeechRecognizer 초기화 ------------------------------------------------------------------------
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(requireContext()) // 수정: applicationContext 대신 requireContext() 사용
         speechRecognizer.setRecognitionListener(object : RecognitionListener {
             override fun onReadyForSpeech(params: Bundle?) {
                 // 음성 인식 준비 완료
-                Toast.makeText(requireContext(), "음성 인식 시작", Toast.LENGTH_SHORT).show() // 수정: applicationContext 대신 requireContext() 사용
+
+                if (!duplication_check) {
+                    Toast.makeText(requireContext(), "음성 인식 시작", Toast.LENGTH_SHORT).show() // 수정: applicationContext 대신 requireContext() 사용
+                    duplication_check = true
+                }
+                else{
+                    Toast.makeText(requireContext(), "음성 인식 중간 이어서~", Toast.LENGTH_SHORT).show() // 수정: applicationContext 대신 requireContext() 사용
+                }
+
             }
 
             override fun onBeginningOfSpeech() {}
@@ -202,7 +236,7 @@ class AudioFragment : Fragment() {
 
             override fun onError(error: Int) {
                 // 음성 인식 오류 처리
-                Toast.makeText(requireContext(), "음성 인식 오류 발생", Toast.LENGTH_SHORT).show() // 수정: applicationContext 대신 requireContext() 사용
+                // Toast.makeText(requireContext(), "음성 인식 오류 발생", Toast.LENGTH_SHORT).show() // 수정: applicationContext 대신 requireContext() 사용
             }
 
             override fun onResults(results: Bundle?) {
@@ -231,49 +265,39 @@ class AudioFragment : Fragment() {
 
             }
 
-            override fun onPartialResults(partialResults: Bundle?) {
-                Log.d("순간결과 출력", " 이함수가 호출은 되는겨??")
-                val matches = partialResults?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-                if (matches != null && matches.isNotEmpty()) {
-                    val text = matches[0]
-//                    stttext.setText(text)
-                    Log.d("순간결과 출력", text)
-                }
-
-
-            }
+            override fun onPartialResults(partialResults: Bundle?) {}
 
             override fun onEvent(eventType: Int, params: Bundle?) {}
         })
 
-        // stt 버튼 클릭
-        sttButton.setOnClickListener {
-            if (!isListening) { // 음성 인식 중이 아닌 경우에만 음성인식 시작
-                isListening = true
-                val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR")
-//                intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, 5000); // 녹음 기한 5초
-//                intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, 5000); // 녹음 기한 5분
-
-                speechRecognizer.startListening(intent)
-                sttButton.text = "stt 녹음중지" // stt녹음 시작 시 버튼 텍스트 변경
-
-                Log.d("isListening", isListening.toString())
-                Log.d("stt", "stt 음성인식 시작")
-
-            } else { // 음성 인식 중이면 음성인식 종료
-                isListening = false
-                speechRecognizer.cancel()
-                sttButton.text = "stt 녹음시작" // stt녹음 중지 시 버튼 텍스트 변경
-                val text = recognizedText.joinToString(separator = " ")
-                stttext.setText(text)
-                recognizedText.clear() // 다음 인식을 위해 비우기
-
-                Log.d("isListening", isListening.toString())
-                Log.d("stt", "stt 음성인식 중단")
-            }
-        }
+//        // stt 버튼 클릭  - 녹음 버튼 통합으로 주석처리
+//        sttButton.setOnClickListener {
+//            if (!isListening) { // 음성 인식 중이 아닌 경우에만 음성인식 시작
+//                isListening = true
+//                val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+//                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+//                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR")
+//                //intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, 5000); // 녹음 기한 5초
+//                //intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, 5000); // 녹음 기한 5분
+//
+//                speechRecognizer.startListening(intent)
+//                sttButton.text = "stt 녹음중지" // stt녹음 시작 시 버튼 텍스트 변경
+//
+//                Log.d("isListening", isListening.toString())
+//                Log.d("stt", "stt 음성인식 시작")
+//
+//            } else { // 음성 인식 중이면 음성인식 종료
+//                isListening = false
+//                speechRecognizer.cancel()
+//                sttButton.text = "stt 녹음시작" // stt녹음 중지 시 버튼 텍스트 변경
+//                val text = recognizedText.joinToString(separator = " ")
+//                stttext.setText(text)
+//                recognizedText.clear() // 다음 인식을 위해 비우기
+//
+//                Log.d("isListening", isListening.toString())
+//                Log.d("stt", "stt 음성인식 중단")
+//            }
+//        }
 
 
         // stt 텍스트표기될 위치
@@ -437,75 +461,80 @@ class AudioFragment : Fragment() {
     }
 
 
-    private var recorder: MediaRecorder? = null
-    private var outputFile: File? = null
 
-    private fun startRecording() {
-        //기본 분류 작동
-        audioHelper.startAudioClassification()
-
-        //별개 녹음 작동
-        try {
-            // 녹음 파일 생성
-            val fileName = "${System.currentTimeMillis()}.3gp"
-            // 녹음 파일의 이름을 생성합니다. 현재 시간을 기준으로 파일명을 지정하고 확장자는 ".3gp"를 사용합니다.
-
-            val filePath = "${requireActivity().externalCacheDir?.absolutePath}/$fileName"
-            // 녹음 파일이 저장될 경로를 생성합니다. 외부 캐시 디렉토리에 파일을 저장하기 위함입니다.
-
-            outputFile = File(filePath)
-            // 생성된 파일 경로를 이용해 File 객체를 생성합니다.
-            MediaRecorder().apply { //MediaRecorder 객체를 생성하고, 녹음을 시작합니다
-                setAudioSource(MediaRecorder.AudioSource.MIC) // 마이크 사용
-                setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-                setOutputFile(outputFile?.absolutePath)
-                setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-                prepare()
-                start() // 녹음 시작
-            }
-            isRecording = true
-            Log.d("AudioFragment", "녹음 시작")
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.d("AudioFragment", "녹음 시작 실패")
-        }
-    }
-
-    private fun stopRecording() {
-        //기본분류 작동
-        audioHelper.stopAudioClassification()
-
-        //별개 녹음 작동
-        try {
-            if (isRecording && outputFile != null) {
-                recorder?.apply {
-                    stop() // 녹음 중지
-                    release() // 녹음 리소스 해제
-                }
-                addRecordingToMediaLibrary() // 녹음된 파일을 미디어 라이브러리에 추가
-                isRecording = false
-                Log.d("AudioFragment", "녹음 중지")
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.d("AudioFragment", "녹음 중지실패")
-        }
-
-
-
-    }
-
-    // 미디어 라이브러리에 녹음된 파일을 추가
-    private fun addRecordingToMediaLibrary() {
-        // 녹음된 파일을 미디어 라이브러리에 추가
-        MediaScannerConnection.scanFile(
-            requireContext(),
-            arrayOf(outputFile?.absolutePath),
-            arrayOf("audio/*"),
-            null
-        )
-    }
-
+// 녹음 버튼 통합으로 임시 주석 처리-------------------------------------------------------------------------------------
+//
+//
+//    private var recorder: MediaRecorder? = null
+//    private var outputFile: File? = null
+//
+//
+//    private fun startRecording() {
+//        //기본 분류 작동
+//        audioHelper.startAudioClassification()
+//
+//        //별개 녹음 작동
+//        try {
+//            // 녹음 파일 생성
+//            val fileName = "${System.currentTimeMillis()}.3gp"
+//            // 녹음 파일의 이름을 생성합니다. 현재 시간을 기준으로 파일명을 지정하고 확장자는 ".3gp"를 사용합니다.
+//
+//            val filePath = "${requireActivity().externalCacheDir?.absolutePath}/$fileName"
+//            // 녹음 파일이 저장될 경로를 생성합니다. 외부 캐시 디렉토리에 파일을 저장하기 위함입니다.
+//
+//            outputFile = File(filePath)
+//            // 생성된 파일 경로를 이용해 File 객체를 생성합니다.
+//            MediaRecorder().apply { //MediaRecorder 객체를 생성하고, 녹음을 시작합니다
+//                setAudioSource(MediaRecorder.AudioSource.MIC) // 마이크 사용
+//                setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+//                setOutputFile(outputFile?.absolutePath)
+//                setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+//                prepare()
+//                start() // 녹음 시작
+//            }
+//            isRecording = true
+//            Log.d("AudioFragment", "녹음 시작")
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//            Log.d("AudioFragment", "녹음 시작 실패")
+//        }
+//    }
+//
+//    private fun stopRecording() {
+//        //기본분류 작동
+//        audioHelper.stopAudioClassification()
+//
+//        //별개 녹음 작동
+//        try {
+//            if (isRecording && outputFile != null) {
+//                recorder?.apply {
+//                    stop() // 녹음 중지
+//                    release() // 녹음 리소스 해제
+//                }
+//                addRecordingToMediaLibrary() // 녹음된 파일을 미디어 라이브러리에 추가
+//                isRecording = false
+//                Log.d("AudioFragment", "녹음 중지")
+//            }
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//            Log.d("AudioFragment", "녹음 중지실패")
+//        }
+//
+//
+//
+//    }
+//
+//    // 미디어 라이브러리에 녹음된 파일을 추가
+//    private fun addRecordingToMediaLibrary() {
+//        // 녹음된 파일을 미디어 라이브러리에 추가
+//        MediaScannerConnection.scanFile(
+//            requireContext(),
+//            arrayOf(outputFile?.absolutePath),
+//            arrayOf("audio/*"),
+//            null
+//        )
+//    }
+//-------------------------------------------------------------------------------------
 
     override fun onResume() {
         super.onResume()
@@ -520,7 +549,7 @@ class AudioFragment : Fragment() {
             audioHelper.startAudioClassification()
         }
 
-        Log.e("온리슘실행", "녹음 실행중")
+        Log.e("온리슘실행", "자동분류 실행중")
     }
 
     override fun onPause() {
@@ -528,7 +557,7 @@ class AudioFragment : Fragment() {
         if (::audioHelper.isInitialized ) {
             audioHelper.stopAudioClassification()
         }
-        Log.e("온퍼즈실행", "녹음 중단중")
+        Log.e("온퍼즈실행", "자동분류 중단중")
     }
 
     override fun onDestroyView() {
