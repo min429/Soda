0f062e8e3819a2d9ab58d1cd429dbc16f806008f
min429 <lsm990429@gmail.com>
2023-07-25 09:02:57 +0900

    앱 launcher icon, 패키지 이름 변경

---
+++ app/src/main/java/com/soda/soda/fragments/AudioFragment.kt
@@ -0,0 +1,341 @@
+/*
+ * Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.soda.soda.fragments
+
+import android.content.Intent
+import android.media.AudioRecord
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.soda.soda.helper.AudioClassificationHelper
+import com.soda.soda.R
+import com.soda.soda.databinding.FragmentAudioBinding
+import com.soda.soda.helper.TextMatchingHelper
+import com.soda.soda.service.ForegroundService
+import com.soda.soda.ui.ProbabilitiesAdapter
+import org.tensorflow.lite.support.label.Category
+
+
+//stt
+import android.speech.RecognitionListener
+import android.speech.SpeechRecognizer
+import android.os.Handler
+import android.os.Looper
+import androidx.navigation.Navigation
+import com.soda.soda.fragments.SettingFragment.Companion.isMyServiceRunning
+
+
+private const val TAG = "AudioFragment"
+
+interface AudioClassificationListener {
+    fun onError(error: String)  // 오류 발생 시 호출
+    fun onResult(results: List<Category>, inferenceTime: Long)  // 결과가 도착하면 호출
+}
+
+class AudioFragment : Fragment() {
+    // 뷰 바인딩을 위한 변수와 어댑터 초기화
+    private var _fragmentBinding: FragmentAudioBinding? = null
+    private val fragmentAudioBinding get() = _fragmentBinding!!
+
+    private var recordingDotCount = 0
+    private val recordingHandler = Handler(Looper.getMainLooper())
+    private val recordingRunnable = object : Runnable {
+        override fun run() {
+            recordingDotCount = (recordingDotCount + 1) % 4
+            fragmentAudioBinding.recordingText.text = "녹음중" + ".".repeat(recordingDotCount)
+            recordingHandler.postDelayed(this, 500)
+        }
+    }
+
+    // stt 용
+    private lateinit var speechRecognizer: SpeechRecognizer
+    private var duplication_check = false // 음성인식 시작 중복 처리 플래그 변수
+    private var result_check = false // 음성인식 결과 처리 플래그 변수
+    private var record_cancel = false // 음성인식 취소 처리 플래그 변수
+    val resultText = mutableListOf<String>() // 결과 stt 표기용
+
+    private val audioClassificationListener = object : AudioClassificationListener {
+        // 결과가 도착하면 호출되며, 전달된 결과 및 추론 시간 정보를 기반으로 어댑터를 업데이트합니다.
+        override fun onResult(results: List<Category>, inferenceTime: Long) {
+            requireActivity().runOnUiThread {
+                adapter.categoryList = results
+                if(!results.isEmpty())
+                    AudioClassificationHelper.label = TextMatchingHelper.textMatch(results[0])
+                adapter.notifyDataSetChanged()
+            }
+        }
+        // 오류 발생 시 호출되며 Toast 메시지 출력 후 어댑터를 초기화하여 화면에 표시되는 확률값을 모두 0으로 만듭니다.
+        override fun onError(error: String) {
+            requireActivity().runOnUiThread {
+                Toast.makeText(requireContext(), error, Toast.LENGTH_SHORT).show()
+                adapter.categoryList = emptyList()
+                adapter.notifyDataSetChanged()
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // 뷰 바인딩 초기화
+        _fragmentBinding = FragmentAudioBinding.inflate(inflater, container, false)
+        return fragmentAudioBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // 어댑터 설정
+        fragmentAudioBinding.recyclerView.adapter = adapter
+
+        // AudioClassificationHelper 객체 생성 및 초기화
+        audioHelper = AudioClassificationHelper(
+            requireContext(),
+            audioClassificationListener
+        )
+
+        // 스위치 + STT 녹음 버튼------------------------------------------------------------------------
+        val recordButton = view.findViewById<Button>(R.id.record_button)
+        val RECORDING_TIMEOUT = 5000 // 녹음 타임아웃 시간 (5초)
+        var isEndOfSpeech  = false //인식된 소리가 없을 때 처리 플래그 변수
+        val autoSwitchStateValue = SettingFragment.autoSwitchState
+
+        // 녹음 버튼 클릭-----------------------------------------------------------------------
+        recordButton.setOnClickListener {
+            if(!result_check){ // 결과 처리중일 때 -> 아직 결과 안나온 동안
+                if (!isListening) { // 음성 인식 중이 아닌 경우에만 음성인식 시작
+                    //분류 중단
+                    adapter.categoryList = emptyList()
+                    adapter.notifyDataSetChanged()
+                    stopRecording() // -> 녹음중단
+
+                    // 녹음시작
+                    isListening = true
+                    startSTT()
+                    fragmentAudioBinding.recordingText.visibility = View.VISIBLE
+                    startRecordingAnimation()
+                    recordButton.setBackgroundResource(R.drawable.record_stop) // 녹음 시작 시 이미지 변경
+                    result_check=true//터치 막기
+                    Log.d(TAG, "stt 음성인식 시작")
+
+                    // 5초 타임아웃 처리
+                    recordingHandler.postDelayed({
+                        if(!isEndOfSpeech && !record_cancel){
+                            Log.e(TAG, "stt 소리X 5초 지남")
+                            speechRecognizer.stopListening()
+                            fragmentAudioBinding.recordingText.visibility = View.GONE
+                            stopRecordingAnimation()
+                            recordButton.setBackgroundResource(R.drawable.record_start) // 녹음 중지 시 이미지 변경
+                            val text = resultText.joinToString(separator = " ")
+                            resultText.clear()// 다음 stt인식을 위해 비우기
+                            if(autoSwitchStateValue){
+                                startRecording()
+                                Log.d(TAG, "스위치 on 일때 자동녹음 레코딩 시작")
+                            }
+                            result_check=false
+                            isListening = false
+                            duplication_check= false
+                            Toast.makeText(requireContext(), "5초 이상 말소리가 감지되지 않음", Toast.LENGTH_SHORT).show() // 수정: applicationContext 대신 requireContext() 사용
+
+                        }
+                    }, RECORDING_TIMEOUT.toLong())
+
+                    record_cancel = false
+                }
+                else { // 음성 인식 중이면 음성인식 종료시퀀스 -> 프래그먼트 이동
+
+                    Log.d(TAG, "stt 음성인식 중단")
+                    speechRecognizer.stopListening()
+                    fragmentAudioBinding.recordingText.visibility = View.GONE
+                    stopRecordingAnimation()
+                    recordButton.setBackgroundResource(R.drawable.record_start) // 녹음 중지 시 이미지 변경
+                    val text = resultText.joinToString(separator = " ")
+                    resultText.clear() // 다음 stt인식을 위해 비우기
+                    //startRecording() //-> 녹음 재개 - 주석 처리해야 stt 중지되는 부분 처리할 수 있음
+                    isListening = false
+                    duplication_check= false
+                    navigateToFragment(SttFragment(),text)//stt 처리 끝났을 시 SttFragment로 교체
+                }
+            }
+            else{ // 강제로 취소 버튼 누를 때
+                Log.e(TAG, "stt 인식 취소")
+                speechRecognizer.stopListening()
+                fragmentAudioBinding.recordingText.visibility = View.GONE
+                stopRecordingAnimation()
+                recordButton.setBackgroundResource(R.drawable.record_start) // 녹음 중지 시 이미지 변경
+                val text = resultText.joinToString(separator = " ")
+                resultText.clear()// 다음 stt인식을 위해 비우기
+                if(autoSwitchStateValue){
+                    startRecording()
+                    Log.d(TAG, "스위치 on 일때 자동녹음 레코딩 시작")
+                }
+                result_check=false
+                isListening = false
+                duplication_check= false
+                record_cancel =true
+
+                Toast.makeText(requireContext(), "인식취소", Toast.LENGTH_SHORT).show() // 수정: applicationContext 대신 requireContext() 사용
+
+            }
+            Log.d(TAG, "취소 여부 확인 확인 = "+ record_cancel)
+        }
+
+        // SpeechRecognizer 초기화 ------------------------------------------------------------------------
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(requireContext()) // 수정: applicationContext 대신 requireContext() 사용
+        speechRecognizer.setRecognitionListener(object : RecognitionListener {
+            override fun onReadyForSpeech(params: Bundle?) {
+                // 음성 인식 준비 완료
+                if (!duplication_check) {
+                    Toast.makeText(requireContext(), "음성 인식 시작", Toast.LENGTH_SHORT).show() // 수정: applicationContext 대신 requireContext() 사용
+                    duplication_check = true
+                }
+                else{
+                    Toast.makeText(requireContext(), "음성 인식 중간 이어서~", Toast.LENGTH_SHORT).show() // 수정: applicationContext 대신 requireContext() 사용
+                }
+            }
+
+            override fun onBeginningOfSpeech() {} //동작 시작시 무조건 호출
+
+            override fun onRmsChanged(rmsdB: Float) {} // 계속 호출되는 함수
+
+            override fun onBufferReceived(buffer: ByteArray?) {}
+
+            override fun onEndOfSpeech() { //동작 종료시 무조건 호출
+                Log.d(TAG, "stt onEndOfSpeech ")
+            }
+
+            override fun onError(error: Int) {
+                //Toast.makeText(requireContext(), "음성 인식 오류 발생", Toast.LENGTH_SHORT).show() // 수정: applicationContext 대신 requireContext() 사용
+            }
+
+            override fun onResults(results: Bundle?) {
+                if (!record_cancel){ //인식 취소 안됬을 때만 아래 코드 실행되도록
+                    Log.d(TAG, "stt 처리 완료")
+
+                    // 녹음완료 표시
+                    isEndOfSpeech = true
+                    result_check=false
+                    //결과 출력 버튼으로 변경
+                    recordButton.setBackgroundResource(R.drawable.play_button) // 녹음 중지 시 이미지 변경
+                    stopRecordingAnimation()
+                    fragmentAudioBinding.recordingText.text = "녹음완료"
+
+                    // 인식된 결과 처리
+                    val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                    if (matches != null && matches.isNotEmpty()) {
+                        resultText.add(matches[0])
+                    }
+
+                    if(isListening) {
+                        Log.d(TAG, " stt 처리 재시작")
+                        startSTT()
+                        fragmentAudioBinding.recordingText.visibility = View.VISIBLE
+                    }
+                }
+            }
+
+            override fun onPartialResults(partialResults: Bundle?) {}
+
+            override fun onEvent(eventType: Int, params: Bundle?) {}
+        })
+    }
+
+    override fun onDestroyView() {
+        _fragmentBinding = null
+        super.onDestroyView()
+        speechRecognizer.destroy()
+    }
+
+    private fun startRecordingAnimation() {
+        recordingHandler.post(recordingRunnable)
+    }
+
+    private fun stopRecordingAnimation() {
+        recordingHandler.removeCallbacks(recordingRunnable)
+    }
+
+    // STT 음성 인식을 시작하는 startSTT 함수
+    private fun startSTT() {
+        // 음성 인식 시작
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR")
+        //intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, 10000); // 녹음 완전 종료 후 연장 기한 10초
+        //intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, 10000); // 녹음 중간 공백 연장 기한 5초
+        speechRecognizer.startListening(intent)
+    }
+
+    private fun navigateToFragment(fragment: SttFragment, text: String) {
+        val bundle = Bundle()
+        bundle.putString("text", text)
+        fragment.arguments = bundle
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Make sure that all permissions are still present, since the
+        // user could have removed them while the app was in paused state.
+        if (!PermissionsFragment.hasPermissions(requireContext())) {
+            Navigation.findNavController(requireActivity(), R.id.fragment_container)
+                .navigate(AudioFragmentDirections.actionAudioToPermissions())
+        }
+        else{
+            if(SettingFragment.autoSwitchState){
+                startRecording()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if(!isMyServiceRunning(requireContext(), ForegroundService::class.java)){
+            stopRecording()
+        }
+    }
+
+    companion object {
+        var isListening = false // 음성 인식 중 여부를 추적하는 플래그 변수
+        var audioHelper: AudioClassificationHelper? = null
+        private val adapter by lazy { ProbabilitiesAdapter() }
+        fun startRecording() {
+            if(audioHelper?.getRecorderState() != AudioRecord.RECORDSTATE_RECORDING){
+                audioHelper?.startAudioClassification()
+                Log.d(TAG, "녹음 재개")
+            }
+        }
+        fun stopRecording() {
+            if(audioHelper?.getRecorderState() != AudioRecord.RECORDSTATE_STOPPED){
+                audioHelper?.stopAudioClassification()
+                adapter.categoryList = emptyList()
+                adapter.notifyDataSetChanged()
+                Log.d(TAG, "녹음 중단")
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/soda/soda/helper/AudioClassificationHelper.kt
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.soda.soda.helper
+
+import android.content.Context
+import android.media.AudioRecord
+import android.os.SystemClock
+import android.util.Log
+import com.soda.soda.fragments.AudioClassificationListener
+import org.tensorflow.lite.support.audio.TensorAudio
+import org.tensorflow.lite.task.audio.classifier.AudioClassifier
+import org.tensorflow.lite.task.core.BaseOptions
+import java.util.concurrent.ScheduledThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+import kotlin.properties.Delegates
+
+private const val TAG = "AudioClassificationHelper"
+
+class AudioClassificationHelper(
+    val context: Context,
+    val listener: AudioClassificationListener,
+    var currentModel: String = YAMNET_MODEL, //현재 모델
+    var classificationThreshold: Float = DISPLAY_THRESHOLD,//분류 임계값
+    var overlap: Float = DEFAULT_OVERLAP_VALUE, //오버랩 값
+    var numOfResults: Int = DEFAULT_NUM_OF_RESULTS, //결과 개수
+    var currentDelegate: Int = 0,// 현재 대리자 (CPU, NNAPI)
+    var numThreads: Int = 1 //쓰레드 개수
+
+) {
+    private lateinit var classifier: AudioClassifier
+    private lateinit var tensorAudio: TensorAudio
+//    private lateinit var executor: ScheduledThreadPoolExecutor
+//    private lateinit var soundCheckExecutor: ScheduledThreadPoolExecutor
+    private var bytesRead by Delegates.notNull<Int>()
+
+    private val classifyRunnable = Runnable {
+        classifyAudio()
+    }
+
+    // 새로운 스레드에서 소리 크기 확인 및 진동 울리기
+    private val soundCheckRunnable = Runnable {
+        SoundCheckHelper.soundCheck(tensorAudio, bytesRead, context)
+    }
+
+    init {
+        initClassifier()
+    }
+
+    private fun initClassifier() {
+        // Set general detection options, e.g. number of used threads
+        val baseOptionsBuilder = BaseOptions.builder()
+            .setNumThreads(numThreads)
+
+        // Use the specified hardware for running the model. Default to CPU.
+        // Possible to also use a GPU delegate, but this requires that the classifier be created
+        // on the same thread that is using the classifier, which is outside of the scope of this
+        // sample's design.
+        when (currentDelegate) {
+            DELEGATE_CPU -> {
+                // Default
+                //모델은 CPU에서 실행
+            }
+            DELEGATE_NNAPI -> {
+                baseOptionsBuilder.useNnapi()
+            }
+        }
+
+        // Configures a set of parameters for the classifier and what results will be returned.
+        val options = AudioClassifier.AudioClassifierOptions.builder()
+            .setScoreThreshold(classificationThreshold) //분류할 임계값 설정
+            .setMaxResults(numOfResults) //분류 결과의 최대 개수
+            .setBaseOptions(baseOptionsBuilder.build())
+            .build()
+
+        try {
+            // Create the classifier and required supporting objects
+            classifier = AudioClassifier.createFromFileAndOptions(context, currentModel, options)
+            tensorAudio = classifier.createInputTensorAudio()
+
+            //필요한 객체를 생성하는 분류기 생성
+            recorder = classifier.createAudioRecord()
+            //분류 시작
+
+        } catch (e: IllegalStateException) {
+            listener.onError(
+                "Audio Classifier failed to initialize. See error logs for details"
+            )
+
+            Log.e("AudioClassification", "TFLite failed to load with error: " + e.message)
+        }
+    }
+
+
+    fun startAudioClassification() {
+        // 음성 녹음 중이면 중복 시작 방지
+        Log.d(TAG,  "자동녹음 실행")
+
+        // 녹음 시작
+        recorder.startRecording()
+        executor = ScheduledThreadPoolExecutor(1)
+        soundCheckExecutor = ScheduledThreadPoolExecutor(1)
+        Log.d(TAG, "recorderState: "+getRecorderState())
+
+        // Each model will expect a specific audio recording length. This formula calculates that
+        // length using the input buffer size and tensor format sample rate.
+        // For example, YAMNET expects 0.975 second length recordings.
+        // This needs to be in milliseconds to avoid the required Long value dropping decimals.
+        val lengthInMilliSeconds = ((classifier.requiredInputBufferSize * 1.0f) /
+                classifier.requiredTensorAudioFormat.sampleRate) * 1000  //오디오가 얼마나 길어야 하는지 계산
+
+        interval = (lengthInMilliSeconds * (1 - overlap)).toLong() //오버랩 값으로 반복 간격 계산
+
+        executor.scheduleAtFixedRate(
+            classifyRunnable,   //-> classifyAudio() 호출
+            0,
+            interval,
+            TimeUnit.MILLISECONDS)
+
+        // 소리 크기 확인 및 진동 실행 로직
+        soundCheckExecutor.scheduleAtFixedRate(
+            soundCheckRunnable,
+            0,
+            interval,
+            TimeUnit.MILLISECONDS
+        )
+    }
+
+    private fun classifyAudio() {
+        bytesRead = tensorAudio.load(recorder)
+        var inferenceTime = SystemClock.uptimeMillis()
+        val output = classifier.classify(tensorAudio) //분류 실행
+        inferenceTime = SystemClock.uptimeMillis() - inferenceTime //분류하는데 걸리는 시간 계산
+        listener.onResult(output[0].categories, inferenceTime) //분류 결과를 리스너에게 전달
+    }
+
+    fun stopAudioClassification() {
+        recorder.stop() //오디오 녹음 중지
+        if(executor != null){
+            executor?.shutdownNow() //분류 작업 중지
+            Log.d(TAG, "executor shutdown")
+        }
+        if(soundCheckExecutor != null){
+            soundCheckExecutor.shutdownNow() //데시벨 확인 중지
+            Log.d(TAG, "soundCheckExecutor shutdown")
+        }
+        Log.d(TAG, "자동녹음 중지")
+    }
+
+    fun getRecorderState(): Int {
+        return recorder.recordingState
+    }
+
+    companion object {
+        const val DELEGATE_CPU = 0 //메인 쓰레드에서 작동
+        const val DELEGATE_NNAPI = 1 //NN API 딜리게이트 사용
+        const val DISPLAY_THRESHOLD = 0.3f //결과 출력에 영향을 미치는 임계선 값
+        const val DEFAULT_NUM_OF_RESULTS = 1 //분류 결과의 최대 개수, default 2개 설정
+        const val DEFAULT_OVERLAP_VALUE = 0.5f //반복 실행 간격, default 값은 0.5 설정
+        const val YAMNET_MODEL = "yamnet.tflite" //사용하는 모델, default 값은 YAMNET 설정
+        var interval by Delegates.notNull<Long>()
+        var label: String = "소리분류 결과가 여기에 표시됩니다."
+        lateinit var recorder: AudioRecord
+        lateinit var executor: ScheduledThreadPoolExecutor
+        lateinit var soundCheckExecutor: ScheduledThreadPoolExecutor
+    }
+
+}
