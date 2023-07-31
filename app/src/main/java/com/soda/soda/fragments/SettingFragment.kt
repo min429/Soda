48ab780e6c42836e8f11b0e5880a86c890061bfd
Proverbs13 <ichan0318@naver.com>
2023-07-31 23:35:11 +0900

    사용설명서 다이얼로그화 / 설명서 내용추가

--- app/src/main/java/com/soda/soda/fragments/SettingFragment.kt
+++ app/src/main/java/com/soda/soda/fragments/SettingFragment.kt
@@ -89,20 +89,14 @@ class SettingFragment : Fragment() {
 
         /** 사용설명서 **/
         binding.instructionCard.setOnClickListener {
-            navigateToFragment(UserguideFragment())
+            val userguideFragment = UserguideFragment()
+            userguideFragment.show(parentFragmentManager, "userguide_dialog")
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private fun navigateToFragment(fragment: UserguideFragment) {
-        parentFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .addToBackStack(null)
-            .commit()
     }
 
     companion object {
