b9454f6dbd1d7f0fc4ff4a251269b30617c62713
min429 <lsm990429@gmail.com>
2023-07-31 21:38:41 +0900

    앱을 켰을 때 백그라운드 스위치 상태를 체크해서 바로 백그라운드를 수행할 수 있도록 함

--- app/src/main/java/com/soda/soda/fragments/SettingFragment.kt
+++ app/src/main/java/com/soda/soda/fragments/SettingFragment.kt
@@ -15,10 +15,11 @@ import com.soda.soda.R
 import com.soda.soda.databinding.FragmentSettingBinding
 import com.soda.soda.service.ForegroundService
 import org.tensorflow.lite.examples.audio.fragments.UserguideFragment
+import kotlin.properties.Delegates
 
 private const val TAG = "SettingFragment"
 
-class SettingFragment : Fragment() {
+class SettingFragment : Fragment(){
     private var _binding: FragmentSettingBinding? = null
     private val binding get() = _binding!!
     private lateinit var mainActivity: MainActivity
@@ -43,13 +44,6 @@ class SettingFragment : Fragment() {
         stopServiceIntent = Intent(mainActivity, ForegroundService::class.java)
 
         /** 백그라운드 스위치 **/
-        // Restore switch state from SharedPreferences
-        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
-        val defaultBackgroundSwitchState = false
-        //getString(R.string.saved_switch_state_key)를 통해 strings.xml 파일에 정의된 키 값을 가져오고 sharedPref.getBoolean은 이 키 값에 해당하는 값이
-        //SharedPreferences에 저장되어 있으면 그 값을 반환하고 없으면 defaultSwitchState를 반환함
-        val backgroundSwitchState = sharedPref.getBoolean(getString(R.string.saved_background_switch_state_key), defaultBackgroundSwitchState)
-
         binding.backgroundSwitch.isChecked = backgroundSwitchState
         binding.backgroundSwitch.setOnCheckedChangeListener { _, isChecked ->
             // Save switch state to SharedPreferences when state changes
@@ -57,6 +51,7 @@ class SettingFragment : Fragment() {
             with (sharedPref.edit()) {
                 putBoolean("saved_switch_state_key", isChecked) //"saved_switch_state_key"라는 키 값으로 isChecked라는 값을 SharedPreferences에 저장
                 apply()
+                backgroundSwitchState = isChecked
             }
             if (isChecked) {
                 ContextCompat.startForegroundService(mainActivity, serviceIntent)
@@ -105,9 +100,19 @@ class SettingFragment : Fragment() {
             .commit()
     }
 
+    /** 백그라운드 스위치 상태 설정 **/
+    fun setBackgroundSwitchState(activity: MainActivity){
+        // Restore switch state from SharedPreferences
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+        //getString(R.string.saved_switch_state_key)를 통해 strings.xml 파일에 정의된 키 값을 가져오고 sharedPref.getBoolean은 이 키 값에 해당하는 값이
+        //SharedPreferences에 저장되어 있으면 그 값을 반환하고 없으면 false를 반환함
+        backgroundSwitchState = sharedPref.getBoolean(getString(R.string.saved_background_switch_state_key), false)
+    }
+
     companion object {
         var autoSwitchState: Boolean = true
         var vibrateSwitchState: Boolean = true
+        var backgroundSwitchState by Delegates.notNull<Boolean>()
 
         fun isMyServiceRunning(context: Context, serviceClass: Class<*>): Boolean {
             val manager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
